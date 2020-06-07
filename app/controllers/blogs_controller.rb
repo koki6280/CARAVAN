c05b0f6a2dfeca5d89cb852bf80c92f81f8c7bea
vagrant <vagrant@localhost.localdomain>
2020-06-07 05:53:04 +0000

    [start] 4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
+
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def index
@@ -14,15 +16,23 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
-  private
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+private
 
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
+
 end
